6b24b1fa38d3f06f1f9cd75ed6f87d70a45db2fd
BSINDIA <bsindia@bsindialab05.localdomain>
2019-04-10 08:38:31 -0400

    bug fix

--- src/main/java/it/sella/f24/service/F24OCRService.java
+++ src/main/java/it/sella/f24/service/F24OCRService.java
@@ -753,6 +753,7 @@ public class F24OCRService {
 			nome = nome.replace("**", ";");
 			String[] splitnome = nome.split(";");
 			cognome = splitnome[0];
+			if(nome.length()>1)
 			nome = splitnome[1];
 		}
 		if (!dob.isEmpty()) {
